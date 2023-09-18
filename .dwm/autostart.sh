970f368d510e301a1ed4431420a325f44db88021
randmin <randal.helm@outlook.com>
2023-09-18 00:01:41 -0700

    autostart script for DWM settings

---
+++ .dwm/autostart.sh
@@ -0,0 +1,7 @@
+/home/randmin/.config/_keybind_scripts/set_monitors.sh
+feh --bg-fill -z /home/randmin/OneDrive/software/linux/wallpapers 
+picom -b  
+# compton -b
+exec slstatus &
+/home/randmin/.config/_keybind_scripts/set_volume_baseline.sh
+# exec mozillavpn activate
